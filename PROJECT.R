3229c49b8de4df632cf1bd2b2639d56e923ae966
C-Eatman <65561779+C-Eatman@users.noreply.github.com>
2020-05-18 16:45:07 -0400

    Add files via upload

---
+++ PROJECT.R
@@ -0,0 +1,274 @@
+
+
+
+####this one
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install(c("AnnotationDbi", "DESeq2", "GO.db", "impute", "phyloseq", "preprocessCore"))
+#####
+
+BiocManager::install("WGCNA")
+BiocManager::install("rlang")
+Biocmanager::install("RSQLite")
+warnings()
+####loading expression data########all 12070
+options(stringsAsFactors = FALSE)
+CKDdata=read.table("4Callie.txt")
+dim(CKDdata)
+names(CKDdata)
+datExpr0=as.data.frame(t(CKDdata[,-c(1)]))
+names(datExpr0) = CKDdata$Gene.Name;
+rownames(datExpr0) = names(CKDdata)[-c(1)]
+gsg=goodSamplesGenes(datExpr0, verbose=3);gsg$a10K
+head(CKDdata)
+dim(datExpr0)
+if (!gsg$allOK)
+{
+  # Optionally, print the gene and sample names that were removed:
+  if (sum(!gsg$goodGenes)>0)
+    printFlush(paste("Removing genes:", paste(names(datExpr0)[!gsg$goodGenes], collapse = ", ")));
+  if (sum(!gsg$goodSamples)>0)
+    printFlush(paste("Removing samples:", paste(rownames(datExpr0)[!gsg$goodSamples], collapse = ", ")));
+  # Remove the offending genes and samples from the data:
+  datExpr0 = datExpr0[gsg$goodSamples, gsg$goodGenes]
+}
+####CLUSTERING SAMPLES TO FIND OUTLIERS######
+sampleTree = hclust(dist(datExpr0), method = "average");
+# Plot the sample tree: Open a graphic output window of size 12 by 9 inches
+# The user should change the dimensions if the window is too large or too small.
+sizeGrWindow(12,9)
+#pdf(file = "Plots/sampleClustering.pdf", width = 12, height = 9);
+par(cex = 0.6);
+par(mar = c(0,4,2,0))
+plot(sampleTree, main = "Sample clustering to detect outliers", sub="", xlab="", cex.lab = 1.5,
+     cex.axis = 1.5, cex.main = 2)
+
+####loading clinical trait data############NOT USED
+traitData=read.csv("CKDTraits.csv",header=FALSE)
+dim(traitData)
+traitData
+dim(datExpr0)
+CKDSamples=rownames(datExpr0)
+traitRows=match(CKDSamples,traitData$V1)
+datTraits=traitData[traitRows,-1]
+rownames(traitData)=traitData[traitRows,1]
+###expression data in datExpr0 
+###clinical traits in traitData
+traitData
+# Re-cluster samples
+sampleTree2 = hclust(dist(datExpr0), method = "average")
+# Convert traits to a color representation: white means low, red means high, grey means missing entry
+
+traitColors = labels2colors(traitData$V2);
+# Plot the sample dendrogram and the colors underneath.
+plotDendroAndColors(sampleTree2, traitColors,
+                    groupLabels = names(traitData),
+                    main = "Sample dendrogram and trait heatmap")
+
+###automatic network construction and module detection##########don't use
+
+
+# Choose a set of soft-thresholding powers
+powers = c(c(1:10), seq(from = 12, to=20, by=2))
+# Call the network topology analysis function
+sft = pickSoftThreshold(datExpr0, powerVector = powers, verbose = 5)
+# Plot the results:
+sizeGrWindow(9, 5)
+par(mfrow = c(1,2));
+cex1 = 0.9;
+# Scale-free topology fit index as a function of the soft-thresholding power
+plot(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
+     xlab="Soft Threshold (power)",ylab="Scale Free Topology Model Fit,signed R^2",type="n",
+     main = paste("Scale independence"));
+text(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
+     labels=powers,cex=cex1,col="red");
+# this line corresponds to using an R^2 cut-off of h
+abline(h=0.75,col="red")
+# Mean connectivity as a function of the soft-thresholding power
+plot(sft$fitIndices[,1], sft$fitIndices[,5],
+     xlab="Soft Threshold (power)",ylab="Mean Connectivity", type="n",
+     main = paste("Mean connectivity"))
+text(sft$fitIndices[,1], sft$fitIndices[,5], labels=powers, cex=cex1,col="red")
+
+
+
+
+
+####save module assignment (from automatic blockwise)
+moduleLabels = bwnet$colors
+moduleColors = labels2colors(bwnet$colors)
+MEs = bwnet$MEs;
+geneTree = bwnet$dendrograms[[1]];
+
+
+
+
+# Relabel blockwise modules
+bwLabels = matchLabels(bwnet$colors, moduleLabels);
+# Convert labels to colors for plotting
+bwModuleColors = labels2colors(bwLabels)
+
+table(bwLabels) ###modules identified and module size
+
+
+
+
+####block-wise network construction and module detection#####USE
+
+bwnet = blockwiseModules(datExpr0, maxBlockSize = 6035,
+                         power = 9, TOMType = "unsigned", minModuleSize = 30,
+                         reassignThreshold = 0, mergeCutHeight = 0.25,
+                         numericLabels = TRUE,
+                         saveTOMs = TRUE,
+                         saveTOMFileBase = "CKDTOM-blockwise",
+                         verbose = 3)
+###bwnet$colors contains the module assignment, and bwnet$MEs contains the module eigengenes of the modules
+bwnet$colors
+bwnet$MEs
+### The hierarchical clustering dendrograms (trees) used for the module identification for each block are
+#returned:
+bwnet$dendrograms[[1]] ###number of objects=6018
+bwnet$dendrograms[[2]] ###number of objects=4863
+bwnet$dendrograms[[3]] ###number of objects=1189
+
+###dendograms displayed together with the color assignments:
+# open a graphics window
+sizeGrWindow(6,6)
+
+# Plot the dendrogram and the module colors underneath for block 1
+plotDendroAndColors(bwnet$dendrograms[[1]], bwModuleColors[bwnet$blockGenes[[1]]],
+                    "Module colors", main = "Gene dendrogram and module colors in block 1",
+                    dendroLabels = FALSE, hang = 0.03,
+                    addGuide = TRUE, guideHang = 0.05)
+# Plot the dendrogram and the module colors underneath for block 2
+
+plotDendroAndColors(bwnet$dendrograms[[2]], bwModuleColors[bwnet$blockGenes[[2]]],
+                    "Module colors", main = "Gene dendrogram and module colors in block 2",
+                    dendroLabels = FALSE, hang = 0.03,
+                    addGuide = TRUE, guideHang = 0.05)
+# Plot the dendrogram and the module colors underneath for block 3
+
+plotDendroAndColors(bwnet$dendrograms[[3]], bwModuleColors[bwnet$blockGenes[[3]]],
+                    "Module colors", main = "Gene dendrogram and module colors in block 3",
+                    dendroLabels = FALSE, hang = 0.03,
+                    addGuide = TRUE, guideHang = 0.05)
+
+####not necessary/comparing against self instead of single blockwise
+singleBlockMEs = moduleEigengenes(datExpr0, moduleColors)$eigengenes;
+blockwiseMEs = moduleEigengenes(datExpr0, bwModuleColors)$eigengenes;
+
+single2blockwise = match(names(singleBlockMEs), names(blockwiseMEs))
+signif(diag(cor(blockwiseMEs[, single2blockwise], singleBlockMEs)), 3)
+
+######
+sizeGrWindow(12,9)
+plotDendroAndColors(geneTree,
+                    cbind(moduleColors, bwModuleColors),
+                    c("Single block", "2 blocks"),
+                    main = "Single block gene dendrogram and module colors",
+                    dendroLabels = FALSE, hang = 0.03,
+                    addGuide = TRUE, guideHang = 0.05)
+
+# ???Here we show a more flexible way of plotting several trees and colors on one page
+sizeGrWindow(12,6)
+5
+#pdf(file = "Plots/BlockwiseGeneDendrosAndColors.pdf", wi = 12, he = 6);
+# Use the layout function for more involved screen sectioning
+layout(matrix(c(1:4), 2, 2), heights = c(0.8, 0.2), widths = c(1,1))
+#layout.show(4);
+nBlocks = length(bwnet$dendrograms)
+# Plot the dendrogram and the module colors underneath for each block
+for (block in 1:nBlocks) 
+  plotDendroAndColors(bwnet$dendrograms[[1]], moduleColors[bwnet$blockGenes[[2]]],
+                      "Module colors",
+                      main = paste("Gene dendrogram and module colors in block", block),
+                      dendroLabels = FALSE, hang = 0.03,
+                      addGuide = TRUE, guideHang = 0.05,
+                      setLayout = FALSE)
+
+####3 Relating modules to external clinical trait#########NOT USED
+# Define numbers of genes and samples
+nGenes = ncol(datExpr0);
+nSamples = nrow(datExpr0);
+# Recalculate MEs with color labels
+MEs0 = moduleEigengenes(datExpr0, moduleColors)$eigengenes
+MEs = orderMEs(MEs0)
+moduleTraitCor = cor(MEs, traitData, use = "p",method="spearman");
+moduleTraitPvalue = corPvalueStudent(moduleTraitCor, nSamples)
+
+sizeGrWindow(10,6)
+# DONT USE Will display correlations and their p-values DONT USE BC CATEGORICAL VARIABLE
+textMatrix = paste(signif(moduleTraitCor, 2), "\n(",
+                   signif(moduleTraitPvalue, 1), ")", sep = "");
+dim(textMatrix) = dim(moduleTraitCor)
+par(mar = c(6, 8.5, 3, 3));
+# Display the correlation values within a heatmap plot
+labeledHeatmap(Matrix = moduleTraitCor,
+               xLabels = names(traitData),
+               yLabels = names(MEs),
+               ySymbols = names(MEs),
+               colorLabels = FALSE,
+               colors = blueWhiteRed(50),
+               textMatrix = textMatrix,
+               setStdMargins = FALSE,
+               cex.text = 0.5,
+               zlim = c(-1,1),
+               main = paste("Module-trait relationships"))
+
+
+
+
+####Intramodular analysis: identifying genes with high GS and MM########NOT USED
+module = "red"
+column = match(module, modNames);
+moduleGenes = moduleColors==module;
+sizeGrWindow(7, 7);
+par(mfrow = c(1,1));
+verboseScatterplot(abs(geneModuleMembership[moduleGenes, column]),
+                   
+                   abs(geneTraitSignificance[moduleGenes, 1]),
+                   xlab = paste("Module Membership in", module, "module"),
+                   ylab = "Gene significance for CKD",
+                   main = paste("Module membership vs. gene significance\n"),
+                   cex.main = 1.2, cex.lab = 1.2, cex.axis = 1.2, col = module)
+
+# order the categorical variables #####NOT USED
+traitData$V2 <- as.numeric(factor(traitData$V2, levels=c("CVD", "normal")))
+
+CorLevelPlot(data = traitData,
+             x = c("V2"),
+             y = c(MEs),
+             col = c("darkblue"),
+             cexCorval = 1.5,
+             colCorval = "white",
+             fontCorval = 2,
+             posLab = "bottomleft",
+             rotLabX = 45,
+             posColKey = "top",
+             cexLabColKey = 1.2,
+             scale = TRUE,
+             main = "World Health Organization",
+             colFrame = "white",
+             plotRsquared = FALSE)
+glm(CKDstatus ~ redModule, family=binomial(link='logit'))
+glm(TumourNormalStatus ~ pinkModule, data=data, family=binomial(link='logit'))
+
+CKDstatus = as.data.frame(traitData$V2);
+
+names(CKDstatus) = "Menopausestatus"
+modNames = substring(names(MEs), 3)
+
+geneModuleMembership = as.data.frame(cor(datExpr0, MEs, use = "p"));
+MMPvalue = as.data.frame(corPvalueStudent(as.matrix(geneModuleMembership), nSamples));
+
+names(geneModuleMembership) = paste("MM", modNames, sep="");
+names(MMPvalue) = paste("p.MM", modNames, sep="");
+
+geneTraitSignificance = as.data.frame(cor(datExpr0, CKDstatus, use = "p"));
+GSPvalue = as.data.frame(corPvalueStudent(as.matrix(geneTraitSignificance), nSamples));
+
+names(geneTraitSignificance) = paste("GS.", names(CKDstatus), sep="");
+names(GSPvalue) = paste("p.GS.", names(CKDstatus), sep="");
+module = "red"
+column = match(module, modNames);
+moduleGenes = moduleColors==module
---
+++ Referencedata.R
@@ -0,0 +1,139 @@
+####loading expression data NORMAL
+options(stringsAsFactors = FALSE)
+Ref2data=read.csv("CVDReferencedata.csv")
+dim(Ref2data)
+names(Ref2data)
+datExpr3=as.data.frame(t(Ref2data[,-c(1)]))
+names(datExpr3) = Ref2data$Gene.Name;
+rownames(datExpr3) = names(Ref2data)[-c(1)]
+gsg3=goodSamplesGenes(datExpr3, verbose=3);gsg$a10K
+head(Ref3data)
+dim(datExpr3)
+
+####CLUSTERING SAMPLES TO FIND OUTLIERS######
+sampleTree3 = hclust(dist(datExpr3), method = "average");
+# Plot the sample tree: Open a graphic output window of size 12 by 9 inches
+# The user should change the dimensions if the window is too large or too small.
+sizeGrWindow(12,9)
+#pdf(file = "Plots/sampleClustering.pdf", width = 12, height = 9);
+par(cex = 0.6);
+par(mar = c(0,4,2,0))
+abline(h=14,col="red")
+plot(sampleTree3, main = "Sample clustering to detect outliers-Normal", sub="", xlab="", cex.lab = 1.5,
+     cex.axis = 1.5, cex.main = 2)
+
+names(Ref3data)
+Ref3data<-Ref2data[,-4]
+
+###automatic network construction and module detection
+
+
+# Choose a set of soft-thresholding powers
+powers3 = c(c(1:10), seq(from = 12, to=20, by=2))
+# Call the network topology analysis function
+sft = pickSoftThreshold(datExpr3, powerVector = powers3, verbose = 5)
+# Plot the results:
+sizeGrWindow(9, 5)
+par(mfrow = c(1,2));
+cex1 = 0.9;
+# Scale-free topology fit index as a function of the soft-thresholding power
+plot(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
+     xlab="Soft Threshold (power)",ylab="Scale Free Topology Model Fit,signed R^2",type="n",
+     main = paste("Scale independence"));
+text(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2],
+     labels=powers3,cex=cex1,col="red");
+# this line corresponds to using an R^2 cut-off of h
+abline(h=0.8,col="red")
+# Mean connectivity as a function of the soft-thresholding power
+plot(sft$fitIndices[,1], sft$fitIndices[,5],
+     xlab="Soft Threshold (power)",ylab="Mean Connectivity", type="n",
+     main = paste("Mean connectivity"))
+text(sft$fitIndices[,1], sft$fitIndices[,5], labels=powers3, cex=cex1,col="red")
+
+net3 = blockwiseModules(datExpr3, power = 14,
+                        TOMType = "unsigned", minModuleSize = 15,
+                        reassignThreshold = 0, mergeCutHeight = 0.25,
+                        numericLabels = TRUE, pamRespectsDendro = FALSE,
+                        saveTOMs = TRUE,
+                        saveTOMFileBase = "RefTOM", 
+                        verbose = 3)
+# open a graphics window
+sizeGrWindow(12, 9)
+# Convert labels to colors for plotting
+mergedColors3 = labels2colors(net3$colors)
+# Plot the dendrogram and the module colors underneath
+plotDendroAndColors(net3$dendrograms[[1]], mergedColors3[net3$blockGenes[[1]]],
+                    "Module colors",
+                    main=("Dendrogram of modules in normal data"),
+                    dendroLabels = FALSE, hang = 0.03,
+                    addGuide = TRUE, guideHang = 0.05)
+moduleLabels3 = net3$colors
+moduleColors3 = labels2colors(net3$colors)
+MEs3 = net3$MEs;
+geneTree3 = net3$dendrograms[[1]];
+
+consMEs = net$multiMEs;
+moduleLabels = net$colors;
+# Convert the numeric labels to color labels
+moduleColors = labels2colors(moduleLabels)
+consTree = net$dendrograms[[1]]
+
+net3$colors #module assignments
+net3$MEs #module eigangenes
+####CKD vs Normal
+
+# Rename variables to avoid conflicts
+CKDLabels = moduleLabels1;
+CKDColors = moduleColors1;
+CKDTree = geneTree1;
+CKDMEs = orderMEs(MEs1, greyName = "ME0")
+####The consensus network analysis results are represented by the variables consMEs, moduleLabels, moduleColors, and
+#consTree. We are now ready to relate the female modules to the consensus modules. We calculate the overlaps
+#of each pair of female-consensus modules, and use the Fisher’s exact test (also known as hypergeometric test) to
+#assign a p-value to each of the pairwise overlaps.
+
+# Isolate the module labels in the order they appear in ordered module eigengenes
+CKDModuleLabels = substring(names(CKDMEs), 3)
+NormalModuleLabels = substring(names(MEs3), 3)
+# Convert the numeric module labels to color labels
+CKDModules = labels2colors(as.numeric(CKDModuleLabels))
+NormalModules = labels2colors(as.numeric(NormalModuleLabels))
+# Numbers of female and consensus modules
+nCKDMods = length(CKDModules)
+nNormalMods = length(NormalModules)
+# Initialize tables of p-values and of the corresponding counts
+pTable = matrix(0, nrow = nCKDMods, ncol = nNormalMods);
+CountTbl = matrix(0, nrow = nCKDMods, ncol = nNormalMods);
+# Execute all pairwaise comparisons
+for (fmod in 1:nCKDMods)
+  for (nmod in 1:nNormalMods)
+  {
+    CKDMembers = (CKDColors == CKDModules[fmod]);
+    NormalMembers = (moduleColors3 == NormalModules[nmod]);
+    pTable[fmod, nmod] = -log10(fisher.test(CKDMembers, NormalMembers, alternative = "greater")$p.value);
+    CountTbl[fmod, nmod] = sum(CKDColors == CKDModules[fmod] & moduleColors3 ==
+                                 NormalModules[nmod])
+  }
+
+
+
+###exporting results of the network analysis
+probes = names(datExpr2[[1]]$data)
+probes2Refdata = match(probes, Testdata$Gene.Name)
+#######ONLY USE THIS 
+consMEs.unord = multiSetMEs(multiExpr, universalColors = moduleLabels3, excludeGrey = TRUE)
+consMEs.unord
+kME = list();
+for (set in 1:nSets)
+{
+ 
+  kME[[set]] = corAndPvalue(multiExpr()[[set]]$data, consMEs.unord[[set]]$data);
+}
+kMEmat = rbind(kME[[1]]$cor, kME[[2]]$cor, kME[[1]]$p, kME[[2]]$p);
+MEnames = colnames(consMEs.unord[[1]]$data);
+nMEs = checkSets(consMEs.unord)$nGenes
+dim(kMEmat) = c(nGenes, 6*nMEs)
+rownames(kMEmat) = probes;
+colnames(kMEmat) = spaste(
+  c("kME.set1.", "kME.set2.", "p.kME.set1.", "p.kME.set2.", "Z.kME.meta.", "p.kME.meta"),
+  rep(MEnames, rep(6, nMEs)))
